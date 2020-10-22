35420174ef90371188aadf4ee723b43b019662c5
Leo Fitz <geekmanvel@gmail.com>
2020-10-22 17:06:36 +0800

    爬楼梯算法

---
+++ main/10.22/746.使用最小花费爬楼梯.js
@@ -0,0 +1,24 @@
+/*
+ * @Author: LeoFitz
+ * @Date: 2020-10-22 10:35:30
+ * @LastEditTime: 2020-10-22 10:53:15
+ * @LastEditors: your name
+ * @Description: 
+ * @FilePath: \everyDayFrontEndNnowledgec:\Users\sundan\.leetcode\746.使用最小花费爬楼梯.js
+ */
+/*
+ * @lc app=leetcode.cn id=746 lang=javascript
+ *
+ * [746] 使用最小花费爬楼梯
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} cost
+ * @return {number}
+ */
+var minCostClimbingStairs = function(cost) {
+
+};
+// @lc code=end
+
