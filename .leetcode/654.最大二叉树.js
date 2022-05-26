8716edbbb5a03c3c32b6e213edef3e6c6a1274c8
leo fitz <geekmanvel@gmail.com>
2022-05-26 22:59:46 +0800

    654

--- .leetcode/654.最大二叉树.js
+++ .leetcode/654.最大二叉树.js
@@ -17,8 +17,28 @@
  * @param {number[]} nums
  * @return {TreeNode}
  */
-var constructMaximumBinaryTree = function(nums) {
-
+// 递归解法
+var constructMaximumBinaryTree = function (nums) {
+    // 递归三部曲
+    // 1：确定递归函数的参数和返回值
+    const buildTree = function (arr, left, right) {
+        // 2:确定终止条件
+        if (left > right) return null;
+        let maxIndex = -1;
+        let maxValue = -1;
+        for (let i = left; i <= right; ++i) {
+            if (arr[i] > maxValue) {
+                maxValue = arr[i];
+                maxIndex = i;
+            }
+        }
+        const root = new TreeNode(maxValue);
+        // 确定单层递归条件
+        root.left = buildTree(arr, left, maxIndex - 1);
+        root.right = buildTree(arr, maxIndex + 1, right);
+        return root;
+    }
+    return buildTree(nums, 0, nums.length - 1)
 };
 // @lc code=end
 
