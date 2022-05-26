bc442acb1608a7a94545c68abb6b67969b69140b
Leo Fitz <geekmanvel@gmail.com>
2022-05-26 11:45:09 +0800

    106.构建二叉树

--- .leetcode/106.从中序与后序遍历序列构造二叉树.js
+++ .leetcode/106.从中序与后序遍历序列构造二叉树.js
@@ -28,5 +28,30 @@ var buildTree = function (inorder, postorder) {
     root.right = buildTree(inorder.slice(rootIndex + 1), postorder.slice(rootIndex)); // 创建右节点
     return root;
 };
+// 递归完整版
+var buildTree = function (inorder, postorder) {
+    if (!inorder.length || !postorder.length) return null;
+    // 递归三部曲
+    // 1:确认递归函数参数和返回值，参数：前后序的数组，返回值:二叉树
+    const generateTree = function (before, after) {
+        // 2: 确定终止条件
+        if (!before.length || !after.length) return null;
+        // 根节点是后续数组的最后一个元素
+        const rootVal = after.pop();
+        // 中序遍历根节点的左右数组就是左右子树
+        let rootIndex = before.indexOf(rootVal);
+        // 创建中间节点
+        const root = new TreeNode(rootVal);
+        // 如果之后一个元素，则直接返回，不做其他操作了
+        if (root.length === 1) return root;
+        // 3:确认单层递归条件
+        // 创建左节点
+        root.left = generateTree(before.slice(0, rootIndex), after.slice(0, rootIndex));
+        // 创建右节点
+        root.right = generateTree(before.slice(rootIndex + 1), after.slice(rootIndex));
+        return root;
+    }
+    return generateTree(inorder, postorder);
+};
 // @lc code=end
 
